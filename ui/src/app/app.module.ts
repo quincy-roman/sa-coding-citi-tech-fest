7b515fa29dc306af3e9abc9d6b68b4a0a91d0f5a
Zhaowei Ding <86904225+RetroInferno@users.noreply.github.com>
2023-09-21 18:48:03 -0400

    added push notification service and basic logos

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { TestPageComponent } from './test-page/test-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
