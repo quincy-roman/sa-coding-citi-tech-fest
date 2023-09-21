b8212f88c7306927823cc04a40e9a20c4f3f7c49
metsuryuu <qjroman@gmail.com>
2023-09-21 17:09:00 -0400

    first commit

---
+++ api/src/main/java/io/legendofjava/sacoding/SaCodingCtfHackathonApplication.java
@@ -0,0 +1,13 @@
+package io.legendofjava.sacoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaCodingCtfHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaCodingCtfHackathonApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/io/legendofjava/sacoding/SaCodingCtfHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package io.legendofjava.sacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaCodingCtfHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
