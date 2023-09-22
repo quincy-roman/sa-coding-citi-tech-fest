9c9e048eb3ed6aa8ed320a3535b76ade4ac194d0
Harrison Cooley <harrison_v_cooley@me.com>
2023-09-21 22:54:33 -0400

    Learner Controller

---
+++ api/src/main/java/io/legendofjava/sacoding/Controller/LearnerController.java
@@ -0,0 +1,28 @@
+package io.legendofjava.sacoding.controller;
+
+import io.legendofjava.sacoding.entity.Assignment;
+import io.legendofjava.sacoding.service.LearnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/learner")
+public class LearnerController {
+
+    @Autowired
+    private LearnerService learnerService;
+
+    @GetMapping("/assignments")
+    public ResponseEntity<List<Assignment>> getAssignmentsForStudent(@RequestParam String batchId){
+        List<Assignment> assignmentList= learnerService.getAssignmentsForStudent(batchId);
+        return ResponseEntity.ok(assignmentList);
+    }
+
+
+}
