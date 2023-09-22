6fb5e91f11e22267369c4b2804a763e136911ea7
metsuryuu <qjroman@gmail.com>
2023-09-22 10:45:37 -0400

    removing bad method

--- api/src/main/java/io/legendofjava/sacoding/repository/UserRepository.java
+++ api/src/main/java/io/legendofjava/sacoding/repository/UserRepository.java
@@ -1,11 +1,8 @@
 package io.legendofjava.sacoding.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.legendofjava.sacoding.entity.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
-    List<User> findByLName(String lName);
 }
