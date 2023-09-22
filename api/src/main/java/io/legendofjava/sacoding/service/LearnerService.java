3abd34c493c1ee2e27559ac0913281350049e9bc
Harrison Cooley <harrison_v_cooley@me.com>
2023-09-21 23:42:23 -0400

    getSubmissionsForLearner LearnerService
    Merged Quincy's interface with my changes

--- api/src/main/java/io/legendofjava/sacoding/service/LearnerService.java
+++ api/src/main/java/io/legendofjava/sacoding/service/LearnerService.java
@@ -2,22 +2,19 @@ package io.legendofjava.sacoding.service;
 
 import io.legendofjava.sacoding.entity.Assignment;
 import io.legendofjava.sacoding.entity.Batch;
+import io.legendofjava.sacoding.entity.Submission;
 import io.legendofjava.sacoding.repository.AssignmentRepository;
 import io.legendofjava.sacoding.repository.BatchRepository;
-import io.legendofjava.sacoding.repository.UserRepository;
-import net.bytebuddy.implementation.bind.annotation.AllArguments;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.legendofjava.sacoding.repository.SubmissionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class LearnerService implements LearningManagementService {
 
-    private final UserRepository userRepository;
     private final AssignmentRepository assignmentRepository;
     private final BatchRepository batchRepository;
 
@@ -29,6 +26,11 @@ public class LearnerService implements LearningManagementService {
 
     public Collection<Batch> getBatches(String facilitatorId) {
         return batchRepository.findByFacilitatorId(facilitatorId);
+    }
+
+    @Override
+    public Collection<Submission> getSubmissions(Collection<String> userIds) {
+        return null;
     }
 
     public Collection<Submission> getSubmissionsForLearner(String userId) {
