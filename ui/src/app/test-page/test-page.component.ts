af704509635402c53cbcf916e0738b7e08acd5b3
Zhaowei Ding <86904225+RetroInferno@users.noreply.github.com>
2023-09-21 20:10:12 -0400

    Added generated assets, added appStatusService

--- ui/src/app/test-page/test-page.component.ts
+++ ui/src/app/test-page/test-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PushNotifyService } from '../services/push-notify.service';
+import { AppStatusService } from '../services/app-status.service';
 
 @Component({
   selector: 'app-test-page',
@@ -7,5 +8,8 @@ import { PushNotifyService } from '../services/push-notify.service';
   styleUrls: ['./test-page.component.css'],
 })
 export class TestPageComponent {
-  constructor(public notify: PushNotifyService) {}
+  constructor(
+    public notify: PushNotifyService,
+    public appStatus: AppStatusService
+  ) {}
 }
