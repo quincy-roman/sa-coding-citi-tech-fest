61eb9ea23341ba48f9c23c8b027e8ef99a21f6d0
metsuryuu <qjroman@gmail.com>
2023-09-22 12:30:02 -0400

    adding spring boot annotations

--- api/src/main/java/io/legendofjava/sacoding/SaCodingCtfHackathonApplication.java
+++ api/src/main/java/io/legendofjava/sacoding/SaCodingCtfHackathonApplication.java
@@ -2,7 +2,11 @@ package io.legendofjava.sacoding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class SaCodingCtfHackathonApplication {
 
