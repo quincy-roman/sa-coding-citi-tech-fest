e8dec5e752f2b73adfe638fa1d51e4dfd1fde83f
yasmin yazdi <yyazdi13@gmail.com>
2023-09-22 15:24:34 -0500

    fix events

--- ui/src/app/components/assignment/assignment.component.ts
+++ ui/src/app/components/assignment/assignment.component.ts
@@ -10,6 +10,7 @@ export class AssignmentComponent {
   tableRows: string[][] = [
     ['Row 1, Cell 1', 'Row 1, Cell 2', 'Row 1, Cell 3'],
     ['Row 2, Cell 1', 'Row 2, Cell 2', 'Row 2, Cell 3'],
+    ['Row 2, Cell 1', 'Row 2, Cell 2', 'Row 2, Cell 3'],
     // Add more rows as needed
   ];
 }
